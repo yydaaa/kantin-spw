6fa1072e67817f73f251f1937ee7bdfdb211f882
Afkar-Saga <afkarsukmawanahmad@gmail.com>
2024-10-24 09:36:58 +0700

    layout dashboard

---
+++ components/dashboard/DashboardHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="bg-black text-white flex items-center p-4">
+    <NuxtLink to="/">
+      <div class="font-bold text-lg">Kantin SPW</div>
+    </NuxtLink>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
